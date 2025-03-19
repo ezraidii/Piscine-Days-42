a7de97bcb0574fddc9ee3be79df0f5b60751d9e9
Mariam <your-email@example.com>
2025-03-19 21:33:06 +0000

    DONE

---
+++ C02/ex10/ft_strlcpy.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariam_user <marvin@42.fr>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/17 11:19:18 by mariam_us         #+#    #+#             */
+/*   Updated: 2025/03/19 20:16:35 by mariam_us        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	count;
+
+	count = 0;
+	while (src[count] != '\0')
+	{
+		count++;
+		if (size == 0)
+			return (count);
+		i = 0;
+		while (src[i] != '\0' && i < size - 1)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (count);
+}
