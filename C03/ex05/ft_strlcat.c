3fce052fde30f8cda4527809e4ce8e6c046f0baf
Mariam <your-email@example.com>
2025-03-21 18:15:23 +0000

    DONE

--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -4,9 +4,11 @@
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mariam_user <marvin@42.fr>                 +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
+/*                                                +#+#+#+#+#+   +#+           *
 /*   Created: 2025/03/21 18:19:38 by mariam_us         #+#    #+#             */
-/*   Updated: 2025/03/21 18:25:22 by mariam_us        ###   ########.fr       */
+/*   Updated: 2025/06/04 15:44:53 by mariam_us        ###   ########.fr       */
+/*   Created: 2025/03/20 23:34:22 by mariam_us         #+#    #+#             */
+/*   Updated: 2025/03/21 18:13:47 by mariam_us        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +17,7 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 	unsigned int	i;
 	unsigned int	j;
 	unsigned int	space_left;
+	unsigned int	space_felt;
 
 	i = 0;
 	j = 0;
@@ -28,6 +31,12 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 		dest[i + j] = src[j];
 		j++;
 		space_left--;
+	space_felt = size - i;
+	while (src[j] != '\0' && space_felt > 1)
+	{
+		dest[i + j] = src[j];
+		j++;
+		space_felt--;
 	}
 	if (i < size)
 	{
