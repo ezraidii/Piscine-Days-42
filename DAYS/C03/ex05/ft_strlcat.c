750020e923057dc52148d7f6f32c7d252c4f944c
Mariam <your-email@example.com>
2025-06-04 21:34:33 +0100

    DONE

---
+++ DAYS/C03/ex05/ft_strlcat.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariam_user <marvin@42.fr>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/21 18:19:38 by mariam_us         #+#    #+#             */
+/*   Updated: 2025/03/21 18:25:22 by mariam_us        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	space_left;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0' && i < size)
+	{
+		i++;
+	}
+	space_left = size - i;
+	while (src[j] != '\0' && space_left > 1)
+	{
+		dest[i + j] = src[j];
+		j++;
+		space_left--;
+	}
+	if (i < size)
+	{
+		dest[i + j] = '\0';
+	}
+	return (i + j);
+}
---
+++ DAYS/C07/ex02/ft_ultimate_range.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariam_user <marvin@42.fr>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/17 12:57:05 by mariam_us         #+#    #+#             */
+/*   Updated: 2025/04/18 13:28:00 by mariam_us        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_ultimate_range(int **range, int min, int max)
+{
+	int	size;
+	int	i;
+
+	if (min >= max)
+	{
+		*range = NULL;
+		return (0);
+	}
+	size = max - min;
+	*range = malloc(sizeof(int) * size);
+	if (*range == 0)
+		return (-1);
+	i = 0;
+	while (min < max)
+	{
+		(*range)[i] = min;
+		min++;
+		i++;
+	}
+	return (size);
+}
